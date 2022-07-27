1cf800460b49efbef6800f07950c9a53984f218f
nepalsandesh <sandeshnepal43@gmail.com>
2022-07-27 14:16:49 +0545

    APIView implemented on views and api_root created

--- growup_fitness/urls.py
+++ growup_fitness/urls.py
@@ -1,20 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from members import views
-from rest_framework.routers import DefaultRouter
 
-
-router =  DefaultRouter()
-
-router.register('members', views.MemberViewset, basename='members')
-router.register('expired-members', views.ExpiredMemberViewset, basename='expiredmembers')
-router.register('non-expired-members', views.NonExpiredMemberViewset, basename='nonexpiredmembers')
-router.register('physicalDetails', views.PhysicalDetailViewset, basename='physicaldetails')
-router.register('packageDetail', views.PackageDetailViewset, basename='packagedetail')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('', include('members.urls')),
     
 ]
-    
---
+++ members/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from .views import api_root, MembersView, MemberDetails, ExpiredMembers, NonExpiredMembers
+
+urlpatterns = [
+    path('', api_root),
+    path('members/', MembersView.as_view(), name='members'),
+    path('members/<int:id>/', MemberDetails.as_view(), name='member_details'),
+    path('expired-members/', ExpiredMembers.as_view(), name='expired_members'),
+    path('non-expired-members/', NonExpiredMembers.as_view(), name='non_expired_members'),
+]
+
